ff3b10f9aebacccdfae3739c8e8e96672ea7de3e
Varun Kumar <vrnkmr55@gmail.com>
2018-12-30 02:27:52 +0530

    Actual Count sort
    Last version only printed elements from input array based on occurrence stored in count array.
    This version further modifies the count array.

--- Count Sort.cpp
+++ Count Sort.cpp
@@ -4,21 +4,26 @@
 
 int main()
 {
-std::vector<int> vec={1,2,3,4,5,1,2,3,1,1,8};
-std::vector<int>::iterator it;
-int max = *std::max_element(vec.begin(),vec.end());
-
-std::vector<int> c(max+1,0);
-
-std::cout<<std::endl;
-
-for(it=vec.begin();it!=vec.end();it++)
-c.at(*it)++;
-
-std::cout<<" Sorted Array : ";
-
-for(int i=0;i<=max;i++)
-    for(int j=1;j<=c.at(i);j++)
-        std::cout<<i<<" ";
-
+    std::vector<int> inp={1,2,3,4,5,8,1,2,3,1,2,3,1,1};
+    std::vector<int> c(*std::max_element(inp.begin(),inp.end())+1,0);
+    std::vector<int> res(inp.size(),0);   
+    std::vector<int>::iterator it;
+    
+    for(it=inp.begin();it!=inp.end();it++)
+    c.at(*it)++;
+     
+    for(int i=1;i<c.size();i++)
+    c.at(i)+=c.at(i-1);
+    
+    for(int i=c.size()-1;i>0;i--)
+    c.at(i)=c.at(i-1);
+    
+    
+    for(int i=0;i<inp.size();i++)
+    res.at(c.at(inp.at(i))++)=inp.at(i);
+    
+    std::cout<<"Sorted Array : ";
+    for(auto& i:res)
+    std::cout<<i<<" ";
+    
 }
